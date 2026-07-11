9d7c99a365e01918a7febabcfaf161082a081a84
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2026-07-11 16:36:21 +0500

    Track placeholder secrets file

---
+++ Sashay/keys.swift
@@ -0,0 +1,18 @@
+//
+//  keys.swift
+//  Sashay
+//
+//  Created by Райымбек Омаров on 10.07.2026.
+//
+
+import Foundation
+
+enum keysGenius: String {
+
+    case clientId = ""
+
+    case secret = "2"
+
+    case redirect = "sashay://oauth"
+
+}
