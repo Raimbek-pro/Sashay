811dfaaeae7883f2d4d980d62e7fe8c86d1f88f9
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2026-07-11 15:23:04 +0500

    unit tests with tests added + mock tests + POP

---
+++ SashayTests/SashayTests.swift
@@ -0,0 +1,16 @@
+//
+//  SashayTests.swift
+//  SashayTests
+//
+//  Created by Райымбек Омаров on 11.07.2026.
+//
+
+import Testing
+
+struct SashayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
